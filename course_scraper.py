cc2a392eed798ff07ba1847a559fabacd4d8a7e2
Noah Kaplan <noahkaplan4@gmail.com>
2015-11-22 20:39:20 -0500

    bugs with splitting title and department, backup files, refactor into functions for html and csv

--- course_scraper.py
+++ course_scraper.py
@@ -1,41 +1,76 @@
 import urllib2
-from bs4 import BeautifulSoup
-from utils import is_next_link, is_course_title
+import os
 import re
 import sys
+import time
+
+from bs4 import BeautifulSoup
+
+from secret import base_path
+from utils import is_next_link, is_course_title, backup
 from csv_file import CsvFile
+
 
 harvard_course_catalog_url = "https://coursecatalog.harvard.edu/icb/icb.do?keyword=CourseCatalog&panel=icb.pagecontent695860%3Arsearch%3Fcontext%3D%244%26coreToolId%3D14346%26fq_coordinated_semester_yr%3D%26fq_school_nm%3D%26keyword%3DCourseCatalog%26pageContentId%3Dicb.pagecontent695860%26pageid%3Dicb.page335057%26permissions%3D7%252C8%26q%3D%26remoteAddr%3D4.35.92.19%26requestId%3D660063%26siteId%3Dicb.site69201%26siteType%3D12%26sort%3Dcourse_title%2Basc%26start%3D50%26submit%3DSearch%26topicId%3Dicb.topic749225%26urlRoot%3Dcourse%244.harvard.edu%26userid%3D--------&pageid=icb.page335057&pageContentId=icb.pagecontent695860&view=search&viewParam_context=catalog&viewParam_coreToolId=14346&viewParam_fq_coordinated_semester_yr=&viewParam_fq_school_nm=&viewParam_keyword=CourseCatalog&viewParam_pageContentId=icb.pagecontent695860&viewParam_pageid=icb.page335057&viewParam_permissions=7%2C8&viewParam_q=&viewParam_remoteAddr=4.35.92.19&viewParam_requestId=660063&viewParam_siteId=icb.site69201&viewParam_siteType=12&viewParam_sort=course_title+asc&viewParam_start=25&viewParam_submit=Search&viewParam_topicId=icb.topic749225&viewParam_urlRoot=coursecatalog.harvard.edu&viewParam_userid=--------&viewParam_context=catalog&viewParam_userid=--------&viewParam_keyword=CourseCatalog&viewParam_siteId=icb.site69201&viewParam_siteType=12&viewParam_topicId=icb.topic749225&viewParam_coreToolId=14346&viewParam_urlRoot=coursecatalog.harvard.edu&viewParam_remoteAddr=4.35.92.19&viewParam_permissions=7%2C8&viewParam_pageContentId=icb.pagecontent695860&viewParam_pageid=icb.page335057&viewParam_requestId=661979"
 
-csv_path = '%s/harvard_courses.csv' % sys.path[0]
+html_path = '%s/harvard_courses.html' % base_path
+csv_path = '%s/harvard_courses.csv'   % base_path
 
-f = CsvFile(csv_path, 'w')
 
-while harvard_course_catalog_url:
+def populate_html_file():
 
-    opened_url = urllib2.urlopen(harvard_course_catalog_url)
+    backup(html_path)
 
-    all_courses_soup = BeautifulSoup(opened_url.read(), 'html.parser')
+    f_html = open(html_path, 'w')
 
-    courses = all_courses_soup.findAll(is_course_title)
+    while harvard_course_catalog_url:
 
-    for course in courses:
+        opened_course_catalog = urllib2.urlopen(harvard_course_catalog_url)
 
-        displayed_course_information = course.contents[0]
+        course_catalog_soup = BeautifulSoup(opened_course_catalog.read(), 'html.parser')
 
-        has_department_and_level = re.match('[A-Z0-9. ]+ - ?', displayed_course_information)
+        for course_tag in course_catalog_soup.findAll(is_course_title):
+            f_html.write('%s\n' % str(course_tag))
 
-        if has_department_and_level:
+        next_link_tag = course_catalog_soup.find(is_next_link)
+
+        harvard_course_catalog_url = next_link_tag.get('href') if next_link_tag else None
+
+    f_html.close()
+
+
+def populate_csv_file():
+
+    backup(csv_path)
+
+    f_csv = CsvFile(csv_path, 'w')
+
+    seen_courses = set()
+
+    course_tags_soup = BeautifulSoup(open(html_path), 'html.parser')
+
+    for course_tag in course_tags_soup.findAll('a'):
+
+        displayed_course_information = course_tag.contents[0]
+
+        department_and_level_regex = '[A-Zx0-9 \.\-]+'
+
+        if re.match('%s \- ' % department_and_level_regex, displayed_course_information):
             department_and_level, title = displayed_course_information.split(' - ', 1)
+        elif re.search(' \- %s($|\s)' % department_and_level_regex, displayed_course_information):
+            title, department_and_level = displayed_course_information.rsplit(' - ', 1)
         else:
-            department_and_level, title = '', displayed_course_information
+            title, department_and_level = displayed_course_information, ''
 
-        url = course.get('href')
+        url = course_tag.get('href')
 
-        f.add_row(title, department_and_level, url)
+        if (title, department_and_level) not in seen_courses:
+            f_csv.add_row(title, department_and_level, url)
 
-    next_link_tag = all_courses_soup.find(is_next_link)
+        seen_courses.add((title, department_and_level))
 
-    harvard_course_catalog_url = next_link_tag.get('href') if next_link_tag else None
+    f_csv.close()
 
-f.close()
+
+populate_html_file()
+populate_csv_file()
--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+import time
+import os
+
+from shutil import copyfile
+from secret import base_path
+
+
 def is_next_link(tag):
     return (tag.name == 'a' and
            tag.parent.get('class') and
@@ -8,3 +15,18 @@ def is_course_title(tag):
     return (tag.name == 'a' and
            tag.parent.get('class') and
            'course_title' in tag.parent.get('class'))
+
+def backup(path):
+    backup_directory = '%s/backup' % base_path
+
+    extention_dot_index = path.rfind('.')
+    last_slash_index = path.rfind('/')
+
+    backup_path = '%s%s_%s%s' % (
+        backup_directory,
+        path[last_slash_index:extention_dot_index],
+        int(time.time()),
+        path[extention_dot_index:]
+    )
+
+    copyfile(path, backup_path)
